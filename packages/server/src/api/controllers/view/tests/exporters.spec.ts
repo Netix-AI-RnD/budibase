236c4f57d8b16f53fb2921a7ff9ba002b9b6502c
Adria Navarro <adria@budibase.com>
2023-05-02 13:12:53 +0100

    Add exporter tests

---
+++ packages/server/src/api/controllers/view/tests/exporters.spec.ts
@@ -0,0 +1,31 @@
+import { csv } from "../exporters"
+
+describe("exporters", () => {
+  describe("csv", () => {
+    test("basic rows can be casted", () => {
+      const headers = ["id", "name", "age"]
+      const rows = [
+        { id: 1, name: "Jack", age: 30 },
+        { id: 2, name: "Sam", age: 34 },
+      ]
+
+      const result = csv(headers, rows)
+      expect(result).toEqual(
+        '"id","name","age"\n"1","Jack","30"\n"2","Sam","34"'
+      )
+    })
+
+    test("rows with empty values can be casted", () => {
+      const headers = ["id", "name", "age", "title", "other"]
+      const rows = [
+        { id: 1, name: "Jack", age: 30, title: "Mr" },
+        { id: 2, name: "Sam", age: 34 },
+      ]
+
+      const result = csv(headers, rows)
+      expect(result).toEqual(
+        '"id","name","age","title","other"\n"1","Jack","30","Mr",\n"2","Sam","34",,'
+      )
+    })
+  })
+})
